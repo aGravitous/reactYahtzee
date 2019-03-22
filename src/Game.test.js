378c825a40ce2e9d333a34c3e91134a7330f5785
Chengsi <gaochengsi@gmail.com>
2019-03-22 11:44:40 -0700

    add test to fix tollgeLocked

--- src/Game.test.js
+++ src/Game.test.js
@@ -1,8 +1,18 @@
 import Game from './Game';
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
+
+let wrapper;
 
 // Smoke test for Game
 it('renders without crashing', () => {
     shallow(<Game />);
 });
+
+// does toggleLocked work
+it('dice should lock/unlock on click', () => {
+    wrapper = mount(<Game />);
+    wrapper.setState({ locked: [true, false, false, false, false] });
+    wrapper.find(".Die-locked").simulate("click");
+    expect(wrapper.state().locked).toEqual([false, false, false, false, false]);
+})
