9a6a7cdd2562be6c4fa3da6227ccb51f2f9ef821
Stéphane Dryefus <floatingrock@gmail.com>
2019-03-22 14:58:07 -0700

    Add game.test.js test to determine if rolls can happen below 0 rolls left.

--- src/Game.test.js
+++ src/Game.test.js
@@ -15,4 +15,17 @@ it('dice should lock/unlock on click', () => {
     wrapper.setState({ locked: [true, false, false, false, false] });
     wrapper.find(".Die-locked").simulate("click");
     expect(wrapper.state().locked).toEqual([false, false, false, false, false]);
-})
+});
+
+// Test to make sure that all dice stay locked and cannot
+// be clicked once rollsLeft is at 0.
+// Also tests whether clicking roll reduces rollsLeft by one
+// Also tests whether rolls reaching 1 or less sets dice
+// to all locked.
+it('should not allow unlock if rolls 0 or less', () => {
+    wrapper = mount(<Game />);
+    wrapper.setState({ rollsLeft: 1 });
+    wrapper.find(".Game-reroll").simulate("click");
+    wrapper.find(".Die-locked").first().simulate("click");
+    expect(wrapper.state().locked).toEqual([true, true, true, true, true]);
+});
