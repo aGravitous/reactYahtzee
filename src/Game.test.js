1cf7d5bc3d62ba1da4844f452e5292ef90addd77
Chengsi <gaochengsi@gmail.com>
2019-03-22 16:06:48 -0700

    fix re-score and tested it

--- src/Game.test.js
+++ src/Game.test.js
@@ -3,6 +3,21 @@ import React from "react";
 import { shallow, mount } from "enzyme";
 
 let wrapper;
+// let sets = {scores: {
+//     ones: undefined,
+//     twos: undefined,
+//     threes: undefined,
+//     fours: undefined,
+//     fives: undefined,
+//     sixes: undefined,
+//     threeOfKind: undefined,
+//     fourOfKind: undefined,
+//     fullHouse: undefined,
+//     smallStraight: undefined,
+//     largeStraight: undefined,
+//     yahtzee: undefined,
+//     chance: 0
+//   }}
 
 // Smoke test for Game
 it('renders without crashing', () => {
@@ -28,4 +43,15 @@ it('should not allow unlock if rolls 0 or less', () => {
     wrapper.find(".Game-reroll").simulate("click");
     wrapper.find(".Die-locked").first().simulate("click");
     expect(wrapper.state().locked).toEqual([true, true, true, true, true]);
+});
+
+//Test to make sure that a user cannot re-score the same scoring line
+it('test to check user should not re-score the same scoring line', () => {
+    wrapper = mount(<Game />);
+    wrapper.setState({dice: [1,2,3,4,5], scores: { ...wrapper.state().scores}});
+    
+    wrapper.find(".RuleRow.RuleRow-active").last().simulate("click");
+    expect(wrapper.state().scores.chance).toEqual(15);
+    wrapper.find(".RuleRow.RuleRow-active").last().simulate("click");
+    expect(wrapper.state().scores.chance).toEqual(15);
 });
